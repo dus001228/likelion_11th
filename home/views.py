71d30be41b25c778b0daac67107cde02cc0b175d
yeachan <dus001228@naver.com>
2023-01-12 23:03:05 +0900

    first commit

---
+++ home/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def aboutus(request):
+    return render(request, 'aboutus.html')
+
+def login(request):
+    return render(request, 'login.html')
